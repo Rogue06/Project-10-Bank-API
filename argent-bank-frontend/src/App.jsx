e60d428bb6ea04b4ecc341b005e470d840a15249
Rogue06 <111644419+Rogue06@users.noreply.github.com>
2025-02-12 11:25:18 +0100

    feat: Add Login page component and route

--- argent-bank-frontend/src/App.jsx
+++ argent-bank-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
+import Login from './pages/Login'
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
       </div>
